779e1072cbaf091796add6e1e91e1ec5f3e59b77
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-03-09 18:21:04 -0300

    test

--- backend/index.js
+++ backend/index.js
@@ -57,7 +57,7 @@ app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader(
         'Access-Control-Allow-Origin',
-        'http://events-router-backend.vercel.app'
+        'https://events-router-client.vercel.app/'
     )
     // res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader(
