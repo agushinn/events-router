7b3b8b2b1951627cfa86f4aade1e39fadced0e85
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-03-09 18:35:27 -0300

    test

--- backend/routes/NewsletterRoutes.js
+++ backend/routes/NewsletterRoutes.js
@@ -16,8 +16,12 @@ const { checkAuthMiddleware } = require('../utils/auth')
 const router = express.Router()
 
 router.get('/', checkAuthMiddleware, NewsletterController.getAllEmails)
-router.post('/', NewsletterController.subscribeEmail)
-router.post('/send', NewsletterController.sendEmails)
-router.delete('/unsubscribe', NewsletterController.unsubscribeEmails)
+router.post('/', checkAuthMiddleware, NewsletterController.subscribeEmail)
+router.post('/send', checkAuthMiddleware, NewsletterController.sendEmails)
+router.delete(
+    '/unsubscribe',
+    checkAuthMiddleware,
+    NewsletterController.unsubscribeEmails
+)
 
 module.exports = router
