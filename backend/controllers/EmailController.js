4589eabd5dcf2c1422f94f3f111acc6aa253cecd
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-03-10 16:36:05 -0300

    test

--- backend/controllers/EmailController.js
+++ backend/controllers/EmailController.js
@@ -1,18 +1,3 @@
-// const EmailService = require('../services/EmailService')
-// const ApiController = require('./ApiController')
-
-// const setCredentials = async (req, res) => {
-//     try {
-//         const { credentials: apiKey } = req.body
-//         EmailService.setCredentials({ apiKey })
-//         ApiController.sendSuccessResponse(res, {}, 'Credenciales actualizadas')
-//     } catch (error) {
-//         ApiController.sendErrorResponse(res, error)
-//     }
-// }
-
-// module.exports = { setCredentials }
-
 const EmailService = require('../services/EmailService')
 const ApiController = require('./ApiController')
 
