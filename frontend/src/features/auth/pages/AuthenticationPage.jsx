c8d293d00e49e39464a132e9a0e64c769e7acab7
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-03-11 01:38:07 -0300

    add auth caption

--- frontend/src/features/auth/pages/AuthenticationPage.jsx
+++ frontend/src/features/auth/pages/AuthenticationPage.jsx
@@ -1,7 +1,18 @@
 import { AuthForm } from '@auth/components/AuthForm'
+import styles from '@auth/styles/AuthenticationPage.module.scss'
 
 function AuthenticationPage() {
-    return <AuthForm method={'POST'} />
+    return (
+        <>
+            <p className={styles.authCaption}>
+                Can login with email:{' '}
+                <span className={styles.credentials}>admin@admin.admin</span>{' '}
+                and password: <span className={styles.credentials}>asdasd</span>{' '}
+                to access the admin panel.
+            </p>
+            <AuthForm method={'POST'} />
+        </>
+    )
 }
 
 export { AuthenticationPage }
