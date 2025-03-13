edf5ab6529b8877d85495ad1293c56c163a0ac02
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-03-13 17:09:20 -0300

    add web analytics and speed insights feature

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,4 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '@css/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+        <App />
+        <Analytics />
+        <SpeedInsights />
+    </>
+)
