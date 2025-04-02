ee6afdcd36ddb4452af7148bec47b15648f02022
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-04-02 17:41:19 -0300

    test swagger in vercel

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,22 @@ app.use('/api/v1/events', eventRoutes)
 app.use('/api/v1/newsletters', newsletterRoutes)
 app.use('/api/v1/emails', emailRoutes)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument))
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument))
+app.use('/api-docs', swaggerUi.serve, async (req, res, next) => {
+    return res.send(swaggerUi.generateHTML(openApiDocument))
+})
+
+// app.use(
+//     '/api-docs',
+//     swaggerUi.serve,
+//     swaggerUi.setup(openApiDocument, {
+//         customCssUrl: 'https://unpkg.com/swagger-ui-dist@4/swagger-ui.css',
+//         customJs: [
+//             'https://unpkg.com/swagger-ui-dist@4/swagger-ui-bundle.js',
+//             'https://unpkg.com/swagger-ui-dist@4/swagger-ui-standalone-preset.js',
+//         ],
+//     }),
+// )
 
 // Error handling
 app.use('*', notFoundMiddleware)
