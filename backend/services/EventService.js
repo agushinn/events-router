93deec48e53efcc048f8cefec9f091f085dfa8b3
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-04-04 19:43:15 -0300

    Validate user ownership before allowing resource updates or deletions

--- backend/services/EventService.js
+++ backend/services/EventService.js
@@ -1,10 +1,11 @@
 const EventRepository = require('../repositories/EventRepository')
-const { InvalidParamError } = require('../factory/ErrorsFactory')
+const { InvalidParamError, NotAuthError } = require('../factory/ErrorsFactory')
 const {
     isValidText,
     isValidDate,
     isValidImageUrl,
 } = require('../utils/validation')
+const { clearImage } = require('../utils/fileUtils')
 
 class EventService {
     static async getAllEvents(pageNumber = 1, limitNumber = 0) {
@@ -72,26 +73,34 @@ class EventService {
         }
     }
 
-    static async createEvent(data) {
+    static async createEvent(bodyData, file) {
         let errors = {}
 
-        if (!data.author_id || data.author_id === '') {
+        if (!bodyData.author_id || bodyData.author_id === '') {
             errors.author_id = 'Author is required'
         }
 
-        if (!isValidText(data.title)) {
+        if (!isValidText(bodyData.title)) {
             errors.title = 'Title is invalid'
         }
 
-        if (!isValidImageUrl(data.image)) {
+        if (!bodyData.imageMode || bodyData.imageMode === '') {
+            errors.imageMode = 'Image mode is required'
+        }
+
+        if (bodyData.imageMode === ' file' && !file) {
+            errors.image = 'Image File is required'
+        }
+
+        if (bodyData.imageMode === 'url' && !isValidImageUrl(bodyData.image)) {
             errors.image = 'Image URL is invalid'
         }
 
-        if (!isValidText(data.description)) {
+        if (!isValidText(bodyData.description)) {
             errors.description = 'Description is invalid'
         }
 
-        if (!isValidDate(data.date)) {
+        if (!isValidDate(bodyData.date)) {
             errors.date = 'Date is invalid'
         }
 
@@ -102,29 +111,43 @@ class EventService {
             )
         }
 
-        return await EventRepository.add(data)
+        if (bodyData.imageMode === 'file' && file) {
+            const filePath = file?.path
+            const fileOriginalName = file?.originalname
+            bodyData.image = filePath
+            bodyData.fileName = fileOriginalName
+        }
+
+        return await EventRepository.add(bodyData)
     }
-
-    static async updateEvent(id, data) {
+    static async updateEvent(id, bodyData, file) {
         let errors = {}
-
+        console.log(bodyData)
         if (!id || id === '') {
             errors.id = 'ID is required'
         }
 
-        if (!isValidText(data.title)) {
+        if (!isValidText(bodyData.title)) {
             errors.title = 'Title is invalid'
         }
 
-        if (!isValidImageUrl(data.image)) {
+        if (!bodyData.imageMode || bodyData.imageMode === '') {
+            errors.imageMode = 'Image mode is required'
+        }
+
+        if (
+            bodyData.imageMode === 'url' &&
+            bodyData.image &&
+            !isValidImageUrl(bodyData.image)
+        ) {
             errors.image = 'Image URL is invalid'
         }
 
-        if (!isValidText(data.description)) {
+        if (!isValidText(bodyData.description)) {
             errors.description = 'Description is invalid'
         }
 
-        if (!isValidDate(data.date)) {
+        if (!isValidDate(bodyData.date)) {
             errors.date = 'Date is invalid'
         }
 
@@ -135,15 +158,85 @@ class EventService {
             )
         }
 
-        return await EventRepository.replace(id, data)
+        const eventToUpdate = await EventRepository.get(id)
+        const eventToUpdateAuthor = eventToUpdate.author_id
+        const userTryingToUpdate = bodyData.author_id
+
+        if (eventToUpdateAuthor !== userTryingToUpdate) {
+            throw new NotAuthError(
+                'You are not authorized to update this event',
+            )
+        }
+
+        if (bodyData.imageMode === 'file') {
+            if (file) {
+                const filePath = file.path
+                const fileName = file.originalname
+                bodyData.image = filePath
+                bodyData.fileName = fileName
+                const eventToUpdateImage = eventToUpdate.image
+                const eventToUpdateImageName = eventToUpdate.fileName
+                if (
+                    (eventToUpdateImage &&
+                        eventToUpdateImage !== filePath &&
+                        eventToUpdateImageName &&
+                        eventToUpdateImageName !== fileName) ||
+                    eventToUpdateImageName === fileName
+                ) {
+                    try {
+                        await clearImage(eventToUpdateImage)
+                    } catch (error) {
+                        throw new Error(
+                            `Error eliminando archivo ${eventToUpdateImage}: ${error.message}`,
+                        )
+                    }
+                }
+            } else {
+                delete bodyData.image
+                delete bodyData.fileName
+                delete bodyData.imageMode
+            }
+        } else if (bodyData.imageMode === 'url') {
+            const eventToUpdateImage = eventToUpdate.image
+            const eventToUpdateImageName = eventToUpdate.fileName
+            const oldMode = eventToUpdate.imageMode
+            if (
+                eventToUpdateImage &&
+                eventToUpdateImageName &&
+                oldMode === 'file'
+            ) {
+                try {
+                    await clearImage(eventToUpdateImage)
+                } catch (error) {
+                    throw new Error(
+                        `Error eliminando archivo ${eventToUpdateImage}: ${error.message}`,
+                    )
+                }
+            }
+            bodyData.fileName = ''
+        }
+
+        const updated = await EventRepository.replace(id, bodyData)
+
+        return updated
     }
 
-    static async deleteEvent(id) {
-        if (!id || id === '') {
+    static async deleteEvent(eventId, userId) {
+        if (!eventId || eventId === '') {
             throw new InvalidParamError('ID is required')
         }
 
-        return await EventRepository.remove(id)
+        const eventToUpdate = await EventRepository.get(eventId)
+        const eventToUpdateAuthor = eventToUpdate.author_id.toString()
+        const userTryingToUpdate = userId
+
+        if (eventToUpdateAuthor !== userTryingToUpdate) {
+            throw new NotAuthError(
+                'You are not authorized to delete this event',
+            )
+        }
+
+        return await EventRepository.remove(eventId)
     }
 }
 
