47c44560b124984ee2bc8590919623d3d312a177
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-04-04 12:53:16 -0300

    create image cleaner

--- backend/utils/fileUtils.js
+++ backend/utils/fileUtils.js
@@ -1,18 +1,41 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
 
-async function readData() {
-    const filePath = path.join(__dirname, '../events.json')
-    const data = await fs.readFile(filePath, 'utf8')
-    return JSON.parse(data)
+async function readData(file) {
+    try {
+        const filePath = path.resolve(__dirname, '../', file)
+        const data = await fs.readFile(filePath, 'utf8')
+        return JSON.parse(data)
+    } catch (error) {
+        console.error(`Error reading file ${file}:`, error.message)
+        throw new Error(`Could not read file: ${file}`)
+    }
 }
 
-async function writeData(data) {
-    const filePath = path.join(__dirname, '../events.json')
-    await fs.writeFile(filePath, JSON.stringify(data))
+async function writeData(file, data) {
+    try {
+        const filePath = path.resolve(__dirname, '../', file)
+        await fs.writeFile(filePath, JSON.stringify(data, null, 2))
+        console.log(`File ${file} written successfully.`)
+    } catch (error) {
+        console.error(`Error writing file ${file}:`, error.message)
+        throw new Error(`Could not write file: ${file}`)
+    }
+}
+
+async function clearImage(file) {
+    try {
+        const filePath = path.join(__dirname, '../', file)
+        await fs.unlink(filePath)
+        console.log(`File ${filePath} deleted successfully.`)
+    } catch (error) {
+        console.error(`Error deleting file ${filePath}:`, error.message)
+        throw new Error(`Could not delete file: ${filePath}`)
+    }
 }
 
 module.exports = {
     readData,
     writeData,
+    clearImage,
 }
