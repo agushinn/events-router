dfd6c93806c7a151512d32ac679896314e61e757
Agustin Reyes <areyes@MacBook-Pro-2.local>
2025-04-07 17:18:26 -0300

    improve skeleton

--- frontend/src/features/events/components/Skeleton.jsx
+++ frontend/src/features/events/components/Skeleton.jsx
@@ -2,34 +2,37 @@ import styles from '@events/styles/Skeleton.module.scss'
 
 function Skeleton() {
     return (
-        <div className={styles.events}>
-            <ul className={styles.list}>
-                <li className={styles.item}>
-                    <div className={styles.elements}>
-                        <div className={styles.image}>
-                            <div className={styles.bar}></div>
+        <>
+            <div className={styles.headerSkeleton}>
+                <div className={styles.titleSkeleton}></div>
+                <div className={styles.selectSkeleton}></div>
+            </div>
+            <div className={styles.events}>
+                <ul className={styles.list}>
+                    <li className={`${styles.item} ${styles.itemA}`}>
+                        <div className={styles.image}></div>
+                        <div className={styles.content}>
+                            <div className={styles.title}></div>
+                            <div className={styles.date}></div>
                         </div>
-                        <div className={styles.content}> </div>
-                    </div>
-                </li>
-                <li className={styles.item}>
-                    <div className={styles.elements}>
-                        <div className={styles.image}>
-                            <div className={styles.bar}></div>
+                    </li>
+                    <li className={`${styles.item} ${styles.itemB}`}>
+                        <div className={styles.image}></div>
+                        <div className={styles.content}>
+                            <div className={styles.title}></div>
+                            <div className={styles.date}></div>
                         </div>
-                        <div className={styles.content}> </div>
-                    </div>
-                </li>
-                <li className={styles.item}>
-                    <div className={styles.elements}>
-                        <div className={styles.image}>
-                            <div className={styles.bar}></div>
+                    </li>
+                    <li className={`${styles.item} ${styles.itemC}`}>
+                        <div className={styles.image}></div>
+                        <div className={styles.content}>
+                            <div className={styles.title}></div>
+                            <div className={styles.date}></div>
                         </div>
-                        <div className={styles.content}> </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
+                    </li>
+                </ul>
+            </div>
+        </>
     )
 }
 
